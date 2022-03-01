81d26049c4907ad36020442fd5d8a9dde2f30a8d
Elbay Malik <eleo.666@gmail.com>
2022-03-01 11:20:21 +0100

    Backing Fields to Properties

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.cupcake.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OrderViewModel : ViewModel() {
+    private val _quantity = MutableLiveData<Int>(0)
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
 }
