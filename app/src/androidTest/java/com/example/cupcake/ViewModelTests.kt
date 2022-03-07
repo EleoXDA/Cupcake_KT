b7feb0514e994f44ad471a4f2d1fba94e4a9a3ef
Elbay Malik <eleo.666@gmail.com>
2022-03-07 21:38:30 +0100

    Add first test

--- app/src/androidTest/java/com/example/cupcake/ViewModelTests.kt
+++ app/src/androidTest/java/com/example/cupcake/ViewModelTests.kt
@@ -2,7 +2,10 @@ package com.example.cupcake
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Assert.assertEquals
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -11,5 +14,13 @@ class ViewModelTests {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun quantity_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
 
 }
