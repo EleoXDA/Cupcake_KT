16e59f5280ef607e2358834f139bf743337c49b7
Elbay Malik <eleo.666@gmail.com>
2022-03-07 20:51:51 +0100

    Create test file

---
+++ app/src/androidTest/java/ViewModelTests.kt
@@ -0,0 +1,13 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+
+class ViewModelTests {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+}
